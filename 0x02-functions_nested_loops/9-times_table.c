9a236838f6a647adeab338402bfbab747dc5a2ae
StephenX94 <stephen.lebitsa@icloud.com>
2023-02-22 07:42:43 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 
-/***
+/**
  * times_table - print the 9 times table
  */
 void times_table(void)
@@ -19,13 +19,12 @@ void times_table(void)
 
 			prod = num * mult;
 
-			if = (prod <= 9)
+			if (prod <= 9)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
 
 			_putchar((prod % 10) + '0');
 		}
-		_putchar('\n');
 	}
 }
