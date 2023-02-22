7e90780740779817cd0673790adb35e370239cfa
StephenX94 <stephen.lebitsa@icloud.com>
2023-02-22 03:45:37 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - prints the sign of a number
  * @n: the number of the sign to be printed
- * Retuirn: 1 if number is greater than zero
+ * Return: 1 if number is greater than zero
  * 0 of number is zero
  * -1 if number is less than zero
  */
